d0f10952d326e508a36374a305032f00b3c46357
amm266 <a.m.mohammadi@gmail.com>
2020-01-26 01:07:00 +0330

    ball movement

---
+++ Assets/scripts/BallScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float power;
+
+    private Rigidbody Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rigidbody.AddForce(power, 0, 0);  //move left
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rigidbody.AddForce(-power, 0, 0); //move right
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Rigidbody.AddForce(0, 0, -power); //move forward
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Rigidbody.AddForce(0, 0, power); //move back
+        }
+        
+    }
+}
